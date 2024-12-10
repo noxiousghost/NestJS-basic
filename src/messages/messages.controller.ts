850ba75d3f516ee840fd5be65cbfc3991c09292f
noxiousghost <sapkota.king@gmail.com>
2024-12-10 20:34:06 +0545

    feat: add create message, get message by id and get all message functionality

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,17 +1,39 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 @Controller('/messages')
 export class MessagesController {
+  messagesService: MessagesService;
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
   @Get('/')
-  listMessages() {}
+  async listMessages() {
+    const result = await this.messagesService.findAll();
+    if (!result) {
+      throw new NotFoundException('messages not found');
+    }
+    return result;
+  }
 
   @Post('/')
-  postMessages(@Body() body: CreateMessageDto) {
-    console.log(body);
+  async postMessages(@Body() body: CreateMessageDto) {
+    return await this.messagesService.create(body.content);
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    console.log(id);
+  async getMessage(@Param('id') id: string) {
+    const result = await this.messagesService.findOne(id);
+    if (!result) {
+      throw new NotFoundException('messages not found');
+    }
+    return result;
   }
 }
--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,6 +1,18 @@
+import { handleJson } from '../utils/handleJson.util';
 export class MessagesRepository {
-  async findOne(id: string) {}
-  async findAll() {}
+  async findOne(id: string) {
+    const contents = await handleJson.readJsonFile();
+    return contents[id];
+  }
+  async findAll() {
+    const contents = await handleJson.readJsonFile();
+    return contents;
+  }
 
-  async create(message: string) {}
+  async create(content: string) {
+    const contents = await handleJson.readJsonFile();
+    const id = Math.floor(Math.random() * 999);
+    contents[id] = { id, content };
+    await handleJson.writeJsonFile(contents);
+  }
 }
